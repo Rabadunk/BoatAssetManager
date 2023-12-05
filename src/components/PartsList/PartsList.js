7d78557ba3ddf12789e9521dc1f65d564c6c53c2
jono <rabadunk@gmail.com>
2023-12-05 20:04:57 +1300

    added css for parts list

--- src/components/PartsList/PartsList.js
+++ src/components/PartsList/PartsList.js
@@ -5,6 +5,8 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import PartCard from './PartCard.js';
 
+import './PartsList.css';
+
 // Sample initial parts data
 const initialParts = [
   { name: 'Part1', mass: 10, centerOfMass: { x: 0, y: 0, z: 0 } },
@@ -30,16 +32,23 @@ const PartsList = () => {
 
 
   return (
-    <div>
-      <h1>Parts List</h1>
-      <ul>
+    <Box className="parts-list-container">
+      <h1>Boat Components</h1>
+      <Box>
         {parts.map((part, index) => (
             <div>
                 <PartCard name={part.name} mass={part.mass} centerOfMass={part.centerOfMass}/>
             </div>
         ))}
-      </ul>
-      <Button onClick={() => handleOpen()}>Add Part</Button>
+      </Box>
+
+      <Box className="button-row">
+        <Button className="open-add-part-button button" variant="contained" onClick={() => handleOpen()}>Add Part</Button>
+        <Button className="open-edit-part-button button" variant="contained" onClick={() => handleOpen()}>Edit Part</Button>
+        <Button className="open-delete-part-button button" variant="contained" onClick={() => handleOpen()}>Delete Part</Button>
+      </Box>
+
+
       
       <Modal
         open={open}
@@ -48,14 +57,14 @@ const PartsList = () => {
             <TextField id="outlined-basic" label="Name" variant="outlined" onChange={(event)=>{setNewPartName(event.target.value)}}/>
             <TextField id="outlined-basic" label="Mass" variant="outlined" type="number" onChange={(event)=>{setNewPartMass(event.target.value)}} />
             <TextField id="outlined-basic" label="Center Of Mass" variant="outlined" />
-            <Button onClick={() => handleClose()}>Cancel</Button>
+            <Button variant="contained" onClick={() => handleClose()} >Cancel</Button>
             <Button onClick={() => {
                 handleClose();
                 addPart(newPartName, newPartMass, { x: 1, y: 1, z: 1 });
-            }}>Add</Button>
+            }} variant="contained">Add</Button>
         </Box>
       </Modal>
-    </div>
+    </Box>
   );
 }
 
